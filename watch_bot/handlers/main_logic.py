375a285dbb407407feac6c977bda891b40ec35a3
radio_wavesss <radio_wavesss@lsd.lsd>
2023-11-25 21:33:36 +0300

    some fixes

--- watch_bot/handlers/main_logic.py
+++ watch_bot/handlers/main_logic.py
@@ -36,41 +36,52 @@ async def make_movie_keyboard(movies)-> types.InlineKeyboardMarkup:
     return keyboard
 
 async def get_film(callback: CallbackQuery,bot:Bot):
-    print(callback)
-    await bot.copy_message(callback.from_user.id,from_chat_id=int(config.chat_id.get_secret_value()),message_id=int(callback.data),protect_content=True) 
-                                                                                                                                                                                                                 
+    try:
+
+        await bot.copy_message(callback.from_user.id,from_chat_id=int(config.chat_id.get_secret_value()),message_id=int(callback.data),protect_content=True)
+        await callback.message.delete()
+    except Exception as e:
+        await send_error_to_devs(msg,error=e,error_text="Ошибка при отправке фильма",bot=bot)
 
     #BASIC USER COMMANDS
 #/Film Name
 async def search_film(msg: types.Message, state: FSMContext, bot: Bot):
-    movies=await db.get_movies(msg.text)
-    if len(movies)==0:
-        await msg.answer("Ничего не найдено, но запрос отправлен модераторам для добавления нового фильма:)") 
-        await db.add_search_query(msg.text,msg.from_user.id)
-    else:                                            
-        b=await make_movie_keyboard(movies)
-        await msg.answer(msg.text,reply_markup=b)
-                                                                                                                                       
+    try:
+        movies=await db.get_movies(msg.text)
+        if len(movies)==0:
+            await msg.answer("Ничего не найдено, но запрос отправлен модераторам для добавления нового фильма:)") 
+            await db.add_search_query(msg.text,msg.from_user.id)
+        else:                                            
+            b=await make_movie_keyboard(movies)
+
+            await msg.answer(msg.text,reply_markup=b)
+    except Exception as e:
+        await send_error_to_devs(msg,error=e,error_text="Ошибка при Поиске Фильма",bot=bot)
+
 
 
 
 #/profile
 async def profile(callback: CallbackQuery,bot: Bot):
-    user_info=await db.get_user_info(callback.from_user.id)
+    try:
+        user_info=await db.get_user_info(callback.from_user.id)
     
-    builder = InlineKeyboardBuilder()
-    builder.add(types.InlineKeyboardButton(
-        text="Главная",
-        callback_data="main")
-    )
+        builder = InlineKeyboardBuilder()
+        builder.add(types.InlineKeyboardButton(
+            text="Главная",
+            callback_data="main")
+        )
 
-    if type(user_info) is Exception:
-        await send_error_to_devs(callback.message,error=user_info,error_text="Ошибка при команде /profile",bot=bot)
-    else:
-        text=f"<b>Профиль:</b>\n\nРеферальная ссылка:\n{user_info[3]}\nКоличество рефералов:{user_info[4]}"
-        pic=FSInputFile("res/profile.png")
-        img=types.input_media_photo.InputMediaPhoto(media=pic,caption=text)
-        await callback.message.edit_media(media=img,reply_markup=builder.as_markup())
+        if type(user_info) is Exception:
+            await send_error_to_devs(callback.message,error=user_info,error_text="Ошибка при команде /profile",bot=bot)
+        else:
+            text=f"<b>Профиль:</b>\n\nРеферальная ссылка:\n{user_info[3]}\nКоличество рефералов:{user_info[4]}"
+            pic=FSInputFile("res/profile.png")
+            img=types.input_media_photo.InputMediaPhoto(media=pic,caption=text)
+            await callback.message.edit_media(media=img,reply_markup=builder.as_markup())
+    except Exception as e:
+        await send_error_to_devs(callback.message,error=e,error_text="Ошибка при нажатии кнопки Профиль",bot=bot)
+
 #/help
 def command_builder(msg: types.Message) -> str:
     command_list="<b>Весь список комманд:</b>\n"
@@ -90,8 +101,8 @@ def command_builder(msg: types.Message) -> str:
         text+=gap+add_film_cmd+popular_queries_cmd
     
     if str(msg.from_user.id) in admin_list:
-        make_ads_cmd="/make_ads - Добавить рекламу\n"
-        check_posts_cmd="/check_posts - Все отложенные посты\n"
+        make_ads_cmd="/new_post - Добавить рекламу\n"
+        check_posts_cmd="/posts - Все отложенные посты\n"
         delete_post_cmd="/delete_post - Удалить отложенный пост\n"
         statistics_cmd="/statistics - Количество активных юзеров в день\nСреднее количество активных юзеров в неделю\nАктивные юзеры с рекламой\n"
 
@@ -105,20 +116,24 @@ async def help(msg: types.Message):
 
 #/start
 async def increment_referral(msg: types.Message, command: CommandObject,bot: Bot):
-    referral = command.args
-    if referral is not None:
-        referral=decode_payload(referral)
-        a=await db.increment_referrals(referral)
+    try:
+        referral = command.args
+        if referral is not None:
+            referral=decode_payload(referral)
+            a=await db.increment_referrals(referral)
         
-        b=await db.get_user_info(referral)
-        if b[4]==3:
-            await db.disable_ads(referral)
-            await bot.send_message(referral,"<b>Количество рефералов равно 3. Реклама отключена!</b>")
+            b=await db.get_user_info(referral)
+            if b[4]==3:
+                await db.disable_ads(referral)
+                await bot.send_message(referral,"<b>Количество рефералов равно 3. Реклама отключена!</b>")
 
-        await bot.send_message(referral,"<b>По вашей реферальной ссылке есть новый пользователь!</b>")
-        if isinstance(a, Exception):
-            print(a)
-            await send_error_to_devs(msg,"Ошибка при инкременте реферала",a,bot)
+            await bot.send_message(referral,"<b>По вашей реферальной ссылке есть новый пользователь!</b>")
+            if isinstance(a, Exception):
+                print(a)
+                await send_error_to_devs(msg,"Ошибка при инкременте реферала",a,bot)
+    except Exception as e:
+        await send_error_to_devs(msg,"Ошибка при инкременте реферала",e,bot)
+
 
 async def callback_start(callback: CallbackQuery,bot: Bot):
     try:
